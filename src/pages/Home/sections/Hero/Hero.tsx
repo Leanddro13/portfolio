68e196362d6480dd124c2c3be4756a50b7d0ccf9
Leandro Silva <leanddrosouzasilva@gmail.com>
2025-07-30 16:03:16 -0300

    feat: responsivel

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,19 +1,18 @@
-import { Box, Button, Container, Grid, styled, Typography } from "@mui/material"
+import { Box, Container, Grid, styled, Typography } from "@mui/material"
 import Avatar from "../../../../assets/images/avatar.jpg"
 import DownloadIcon from '@mui/icons-material/Download';
 import EmailIcon from '@mui/icons-material/Email';
+import StyledButton from "../../../../components/StyledButton/StyledButton";
 
-const StyledHero = styled("div")(() => ({
-    backgroundColor: "black",
-    minHeight: "100vh",
-    //paddingTop: theme.spacing(8),
-    //paddingBottom: theme.spacing(8),
+const StyledHero = styled("div")(({theme}) => ({
+    backgroundColor: theme.palette.primary.main,
+    Height: "100vh",
     display: "flex",
-    alignItems: "center",
+    alignItems: "center"
 }));
 
 const StyledImg = styled("img")(() => ({
-    width: "100%",
+    width: "80%",
     maxwidth: 300,
     borderRadius: "50%",
     display: "block",
@@ -26,24 +25,25 @@ const Hero = () => {
         <StyledHero>
             <Container maxWidth="lg">
                 <Grid container spacing={2} alignItems="center">
-                    <Grid item size = {{ xs: 12, md: 4}}>
+                    <Grid item size = {{ xs: 12, md: 5}}>
                         <StyledImg src={Avatar} alt="Avatar"/>
                     </Grid>
-                    <Grid item size = {{ xs: 12, md: 8}}>
-                        <Typography color="primary" variant="h1" textAlign="center">Leandro Silva</Typography>
-                        <Typography color="primary" variant="h2" textAlign="center">Software Engineer</Typography>
+                    <Grid item size = {{ xs: 12, md: 7}}>
+                        <Typography color="primary.contrastText" variant="h1" textAlign="center">Leandro Silva</Typography>
+                        <Typography color="primary.contrastText" variant="h2" textAlign="center">Software Engineer</Typography>
 
-                        <Grid container display="flex" justifyContent="center">
-                            <Grid item xs={12} md={4} display="flex" justifyContent="center">
-                                <Button> <DownloadIcon/> Download CV</Button>
+                        <Grid container display="flex" justifyContent="center" spacing={3}>
+                            <Grid item size = {{ xs: 12, md: 4}} display="flex" justifyContent="center">
+                                <StyledButton>
+                                    <DownloadIcon/> <Typography>Download CV</Typography>
+                                </ StyledButton>
                             </Grid>
-                            <Grid item xs={12} md={4} display="flex" justifyContent="center">
-                                <Button> <EmailIcon/> Contact me</Button>
+                            <Grid item size = {{ xs: 12, md: 4}} display="flex" justifyContent="center">
+                                <StyledButton> 
+                                    <EmailIcon/> <Typography>Contact me</Typography>
+                                </StyledButton>
                             </Grid>
                         </Grid>
-
-                        
-
                     </Grid>
                 </Grid>
             </Container>
